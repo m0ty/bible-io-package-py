e5456b6c76e0c8c67202e5d37e01752e165505bf
Moty Fainer <motyfainer@live.com>
2025-09-22 11:44:42 +0300

    Use BibleBook enum for book references

--- src/bible_io/chapter.py
+++ src/bible_io/chapter.py
@@ -1,11 +1,12 @@
+from .bible_book_enums import BibleBook
 from .word_index import WordIndex
 from .errors import VerseNotFoundError
 from .verse import Verse
 
 
 class Chapter:
-    def __init__(self, book_number: int, chapter_number: int, verses: list[Verse]):
-        self.book_number = book_number
+    def __init__(self, book: BibleBook, chapter_number: int, verses: list[Verse]):
+        self.book = book
         self.chapter_number = chapter_number
         self.verses = verses
 
@@ -14,12 +15,12 @@ class Chapter:
 
     def get_verse(self, verse_number: int) -> Verse:
         if not (1 <= verse_number <= len(self.verses)):
-            raise VerseNotFoundError(self.book_number, self.chapter_number, verse_number)
+            raise VerseNotFoundError(self.book, self.chapter_number, verse_number)
         return self.verses[verse_number - 1]
 
     def find_word(self, word: str) -> list[WordIndex]:
         indices = []
         for verse in self.verses:
             if verse.contains_word(word):
-                indices.append(WordIndex(self.book_number, self.chapter_number, verse.verse_number))
+                indices.append(WordIndex(self.book, self.chapter_number, verse.verse_number))
         return indices
--- src/bible_io/word_index.py
+++ src/bible_io/word_index.py
@@ -1,10 +1,17 @@
 
+from .bible_book_enums import BibleBook
+
 
 class WordIndex:
-    def __init__(self, book_number: int, chapter_number: int, verse_number: int):
-        self.book_number = book_number
+    def __init__(self, book: BibleBook, chapter_number: int, verse_number: int):
+        self.book = book
         self.chapter_number = chapter_number
         self.verse_number = verse_number
 
     def __repr__(self):
-        return f"WordIndex(Book {self.book_number}, Chapter {self.chapter_number}, Verse {self.verse_number})"
+        return (
+            "WordIndex("
+            f"Book {self.book.as_str()}, "
+            f"Chapter {self.chapter_number}, "
+            f"Verse {self.verse_number})"
+        )
