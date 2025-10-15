35f1eec93055dba28b9689c669983f4bc25cee57
Moty Fainer <motyfainer@live.com>
2025-10-15 22:38:21 +0300

    comment str

--- test/conftest.py
+++ test/conftest.py
@@ -8,4 +8,4 @@ from bible_io import Bible
 def bible():
     data_path = Path(__file__).parent / "bible_versions" / "en_kjv.json"
     assert data_path.is_file(), f"Test data not found at {data_path}"
-    return Bible(data_path)  # cast to str if Bible expects a string path
+    return Bible(data_path)
